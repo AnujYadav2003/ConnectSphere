f5bf9484b9aa3314a1862a5574021ccb120d8133
AnujYadav2003 <120973638+AnujYadav2003@users.noreply.github.com>
2024-12-12 19:45:31 +0530

    Update ChatService.java

--- src/main/java/com/example/Connectify/Service/ChatService.java
+++ src/main/java/com/example/Connectify/Service/ChatService.java
@@ -1,47 +1,4 @@
-//package com.example.Connectify.Service;
-//
-//import com.example.Connectify.Repository.ChatRepository;
-//import com.example.Connectify.model.Chat;
-//import com.example.Connectify.model.User;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.time.LocalDateTime;
-//import java.util.List;
-//import java.util.Optional;
-//
-//@Service
-//public class ChatService {
-//@Autowired
-//private UserService userService;
-//@Autowired
-//private ChatRepository chatRepository;
-//    public Chat createChat(User reqUser, User user2) throws Exception
-//    {
-//         Chat isExists=chatRepository.findChatByUsers(reqUser,user2);
-//   if(isExists!=null)
-//       return isExists;
-//
-//   Chat chat=new Chat();
-//   chat.getUsers().add(user2);
-//   chat.getUsers().add(reqUser);
-//   chat.setTimestamp(LocalDateTime.now());
-//   return chatRepository.save(chat);
-//    }
-//
-//    public Chat findChatById(Long chatId)
-//    {
-//      Optional<Chat> opt=chatRepository.findById(chatId);
-//       if(opt.isEmpty())
-//           throw new RuntimeException("Chat not found with id - "+ chatId);
-//       return opt.get();
-//    }
-//
-//    public List<Chat> findUsersChat(Long userId)
-//    {
-//        return  chatRepository.findByUsersId(userId);
-//    }
-//}
+
 
 package com.example.Connectify.Service;
 
