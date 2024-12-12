0ea4426fe8417295279cbb4e2fc26b4d6ff44c95
AnujYadav2003 <120973638+AnujYadav2003@users.noreply.github.com>
2024-12-12 19:44:51 +0530

    Update ChatRepository.java

--- src/main/java/com/example/Connectify/Repository/ChatRepository.java
+++ src/main/java/com/example/Connectify/Repository/ChatRepository.java
@@ -1,20 +1,4 @@
-//package com.example.Connectify.Repository;
-//
-//import com.example.Connectify.model.Chat;
-//import com.example.Connectify.model.User;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.Query;
-//import org.springframework.data.repository.query.Param;
-//
-//import java.util.List;
-//
-//public interface ChatRepository extends JpaRepository<Chat,Long> {
-//
-//    public List<Chat> findByUsersId(Long userId);
-//
-//    @Query("select c from Chat c Where :user1 Member of c.users And : user2 Member of c.users")
-//     public Chat findChatByUsers(@Param("user1")User user,@Param("user2") User user2);
-//}
+
 
 
 package com.example.Connectify.Repository;
@@ -33,8 +17,6 @@ public interface ChatRepository extends JpaRepository<Chat, Long> {
 
     public List<Chat> findByUsersId(Long userId);
 
-
-    // Custom query to find a chat between two users (using User objects)
     @Query("SELECT c FROM Chat c WHERE :user1 MEMBER OF c.users AND :user2 MEMBER OF c.users")
     Chat findChatByUserId(User user1, User user2);
 
