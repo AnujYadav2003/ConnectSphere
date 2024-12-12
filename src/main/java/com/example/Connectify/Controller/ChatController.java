6566a9e0dd6b7fa816ba8949c44e36af6294f24e
AnujYadav2003 <120973638+AnujYadav2003@users.noreply.github.com>
2024-12-12 19:43:40 +0530

    Update ChatController.java

--- src/main/java/com/example/Connectify/Controller/ChatController.java
+++ src/main/java/com/example/Connectify/Controller/ChatController.java
@@ -1,36 +1,3 @@
-//package com.example.Connectify.Controller;
-//
-//import com.example.Connectify.Service.ChatService;
-//import com.example.Connectify.Service.UserService;
-//import com.example.Connectify.model.Chat;
-//import com.example.Connectify.model.User;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//public class ChatController {
-//    @Autowired
-//    private ChatService chatService;
-//    @Autowired
-//    private UserService userService;
-//@PostMapping("/api/chats")
-//    public Chat createChat(@RequestHeader("Authorization")String jwt,
-//       @RequestBody ChatRequest req) throws Exception{
-//        User reqUser=userService.findUserByJwt(jwt);
-//        User user2=userService.getUserById(req.getUserId());
-//        Chat chat = chatService.createChat(reqUser, user2);
-//        return chat;
-//    }
-//
-//    @GetMapping("/api/chats")
-//    public List<Chat> findUsersChat(@RequestHeader("Authorization")String jwt) throws Exception {
-//    User user=userService.findUserByJwt(jwt);
-//        List<Chat> chat = chatService.findUsersChat(user.getId());
-//        return chat;
-//    }
-//}
 
 
 
