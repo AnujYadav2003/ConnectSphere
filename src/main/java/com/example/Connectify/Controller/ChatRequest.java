ee0ed66e8aa9fa0d8452a3fae956f92e321c6e70
AnujYadav2003 <120973638+AnujYadav2003@users.noreply.github.com>
2024-12-12 19:42:48 +0530

    Update ChatRequest.java

--- src/main/java/com/example/Connectify/Controller/ChatRequest.java
+++ src/main/java/com/example/Connectify/Controller/ChatRequest.java
@@ -1,28 +1,7 @@
-//package com.example.Connectify.Controller;
-//
-//import com.example.Connectify.Service.ChatService;
-//import com.example.Connectify.model.Chat;
-//import com.example.Connectify.model.User;
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.RequestBody;
-//
-//@Setter
-//@Getter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class ChatRequest {
-//   private Long userId;
-//}
-
-
 package com.example.Connectify.Controller;
 
 public class ChatRequest {
-   private Long userId; // The ID of the second user to create a chat with
+   private Long userId;
 
    public Long getUserId() {
       return userId;
